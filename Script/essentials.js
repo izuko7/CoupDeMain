d3984da6645c6e68845b8947fb9a188f118d16ae
Campbell Miha <izuko7mha@gmail.com>
2026-05-18 11:50:42 +0000

    suppresion du fichier entrepiseauth et modification des fichiers scripts

--- Script/essentials.js
+++ Script/essentials.js
@@ -1,13 +1,38 @@
 // Affiche le mot de passe au click
-const input1 = document.getElementById('mdp');
-const icon1 = document.querySelector('.toggle-mdp');
+const input1 = document.getElementById("mdp");
+const icon1 = document.querySelector(".toggle-mdp");
 
 function toggleMotdePasse() {
-    if (input1.type === 'password') {
-        input1.type = 'text';
-        icon1.classList.replace('fa-eye', 'fa-eye-slash');
+    if (input1.type === "password") {
+        input1.type = "text";
+        icon1.classList.replace("fa-eye", "fa-eye-slash");
     } else {
-        input1.type = 'password';
-        icon1.classList.replace('fa-eye-slash', 'fa-eye');
+        input1.type = "password";
+        icon1.classList.replace("fa-eye-slash", "fa-eye");
+    }
+}
+
+// Changer de profil pour l'inscription
+
+function selectionProfil(profil, type) {
+    document.querySelectorAll(".profil-item").forEach((i) => i.classList.remove("active"));
+    profil.classList.add("active");
+
+    document.getElementById("form-particulier").style.display = "none";
+    document.getElementById("form-professionnel").style.display = "none";
+    document.getElementById("form-entreprise").style.display = "none";
+    document.getElementById("gauche-particulier").style.display = "none";
+    document.getElementById("gauche-professionnel").style.display = "none";
+    document.getElementById("gauche-entreprise").style.display = "none";
+
+    if (type === "particulier") {
+        document.getElementById("form-particulier").style.display = "block";
+        document.getElementById("gauche-particulier").style.display = "flex";
+    } else if (type === "entreprise") {
+        document.getElementById("form-entreprise").style.display = "block";
+        document.getElementById("gauche-entreprise").style.display = "flex";
+    } else {
+        document.getElementById("form-professionnel").style.display = "block";
+        document.getElementById("gauche-professionnel").style.display = "flex";
     }
 }
